6f5ad6dc400c9ecba08cc128fee53cfb2ff0d3b9
Alessandro Sagratini <alessandro.sagratini@softecspa.it>
2014-11-06 17:08:10 +0100

    check_memcache.pp

---
+++ manifests/check_memcache.pp
@@ -0,0 +1,22 @@
+define nrpe::check_memcache (
+  $host_port  = '',
+  $w          = '1',
+  $c          = '2',
+) {
+
+  $real_host_port = $host_port? {
+    ''      => $name,
+    default => $host_port
+  }
+
+  $host = inline_template("<%= @real_host_port.split(':').at(0) %>")
+  $port = inline_template("<%= @real_host_port.split(':').at(1) %>")
+
+  package {'libcache-memcached-perl': ensure => present,}
+
+  nrpe::check{ "memcache_${host}_${port}":
+    binary_name => 'check_memcached.pl',
+    params      => "-H ${host} -p ${port} -T'>1,>2' -R '<40,<30' -U '>95,>98'"
+  }
+
+}
