f31e19f6cd2a27b7e0108a102f40eeee7260200e
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2015-02-02 17:00:47 +0100

    aggiunto parametri per monitoraggio soglie

--- manifests/check_memcache.pp
+++ manifests/check_memcache.pp
@@ -1,7 +1,11 @@
 define nrpe::check_memcache (
-  $host_port  = '',
-  $w          = '1',
-  $c          = '2',
+  $host_port    = '',
+  $warn_rtime   = '>1',
+  $crit_rtime   = '>2',
+  $warn_hitrate = '<10',
+  $crit_hitrate = '<8',
+  $warn_usage   = '>95',
+  $crit_usage   = '>98',
 ) {
 
   $real_host_port = $host_port? {
@@ -12,16 +16,20 @@ define nrpe::check_memcache (
   $host = inline_template("<%= @real_host_port.split(':').at(0) %>")
   $port = inline_template("<%= @real_host_port.split(':').at(1) %>")
 
-  if !defined(Package['libcache-memcached-perl']) { 
+  if !defined(Package['libcache-memcached-perl']) {
     package {'libcache-memcached-perl':
       ensure => present,
     }
   }
 
+  $thresholds_rtime   = "\'${warn_rtime},${crit_rtime}\'"
+  $thresholds_hitrate = "\'${warn_hitrate},${crit_hitrate}\'"
+  $thresholds_usage   = "\'${warn_usage},${crit_usage}\'"
+
   nrpe::check{ "memcache_${host}_${port}":
-    contrib     => 'true',
-    binaryname  => 'check_memcached.pl',
-    params      => "-H ${host} -p ${port} -T'>1,>2' -R '<10,<8' -U '>95,>98'"
+    contrib    => true,
+    binaryname => 'check_memcached.pl',
+    params     => "-H ${host} -p ${port} -T ${thresholds_rtime} -R ${thresholds_hitrate} -U ${thresholds_usage}"
   }
 
 }
